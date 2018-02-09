9f51f85686028c9f635da68a0e502694c2625f8d
Ubuntu <ubuntu@ip-10-33-174-85.ec2.internal>
2018-02-09 03:20:30 +0000

    Fixing scheduler

--- scheduler.py
+++ scheduler.py
@@ -39,4 +39,15 @@ def kahn_topsort(graph):
     else:                    # if there is a cycle,  
         return []            # then return an empty list
 
+def getDeployableComponents():
+	components = []
+	components.append({'artifact_name':'popeye_appviz', 'artifact_version':'1.0'})
+	components.append({'artifact_name':'popeye_coreservices', 'artifact_version':'1.7'})
+	components.append({'artifact_name':'popeye_anypoint_ui', 'artifact_version':'0.8'})
+	components.append({'artifact_name':'popeye_cloudhub_platform', 'artifact_version':'2.1'})
+	return components
+
+def generateDeploymentOrder
 order = kahn_topsort(getGraph())
+componentsToBeDeployed = getDeployableComponents()
+print generateDeploymentOrder(order, componentsToBeDeployed)
